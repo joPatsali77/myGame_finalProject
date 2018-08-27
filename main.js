2b9af2da731b5b7e54081127519fe9b7890e500e
JO <ioanna.patsali@live.com>
2018-08-27 11:35:59 -0400

    not working at all

--- main.js
+++ main.js
@@ -1,150 +1,66 @@
-console.log ('joanna is connected')
+document.addEventListener('DOMContentLoaded', function(){
+    console.log ('joanna is connected')
 
-const cvs = document.getElementById("snake");
-const ctx = cvs.getContext("2d");
+// when someone clicks on start the game begins
 
-// create the unit
-const box = 32;
+    var button = document.querySelector('.button')
+    button.addEventListener('click', function (evt) {  
+         evt.preventDefault()
+         //code goes here
+        
 
-// load images
-const ground = new Image();
-ground.src = "assets/background.jpg";
-ground.height= 100;
-
-const foodImg = new Image();
-foodImg.src = "assets/donuts.png";
+    })
 
 
-// load audio files
 
-let dead = new Audio();
-let eat = new Audio();
-let up = new Audio();
-let right = new Audio();
-let left = new Audio();
-let down = new Audio();
+// grab background and snake pixel and create a snake div on DOM. 
+// Attach div on background.
+var background = document.querySelectorAll("background")
 
-dead.src = "audio/dead.mp3";
-eat.src = "audio/eat.mp3";
-up.src = "audio/up.mp3";
-right.src = "audio/right.mp3";
-left.src = "audio/left.mp3";
-down.src = "audio/down.mp3";
+var snake = document.querySelectorAll('snake')
 
-// create the snake
+    snake.createElement('div')
+    snake.classList.add('snake')
+    background.appendChild(snake)
 
-let snake = [];
 
-snake[0] = {
-    x : 9 * box,
-    y : 10 * box
-};
+// grab food pixel and create a div. Attach div to background
+var food = document.querySelectorAll("food")
+    food = document.createElement('div')
+    food.classList.add('food')
+    background.appendChild(food)
 
-// create the food
-
-let food = {
-    x : Math.floor(Math.random()*17+1) * box,
-    y : Math.floor(Math.random()*15+3) * box
+// when snake eats the food I want to add food's div to snake's div
+function eating (){
+    for(let i = 0; i < snake.length; i++){    
+        snake.appendChild(food)
+    }
 }
+eating()
 
-// create the score var
-
-let score = 0;
-
-//control the snake
-
-let d;
+//keyboard
+keyboard.Keymap = {
+    37: 'left',
+    38: 'up',
+    39: 'right',
+    40: 'down'
+    };
 
 document.addEventListener("keydown",direction);
 
 function direction(event){
-    let key = event.keyCode;
-    if( key == 37 && d != "RIGHT"){
+    if( event.keyCode == 37){
         left.play();
-        d = "LEFT";
-    }else if(key == 38 && d != "DOWN"){
-        d = "UP";
+    }else if(key == 38){
         up.play();
-    }else if(key == 39 && d != "LEFT"){
-        d = "RIGHT";
+    }else if(key == 39){
         right.play();
-    }else if(key == 40 && d != "UP"){
-        d = "DOWN";
+    }else if(key == 40){
         down.play();
     }
 }
 
-// cheack collision function
-function collision(head,array){
-    for(let i = 0; i < array.length; i++){
-        if(head.x == array[i].x && head.y == array[i].y){
-            return true;
-        }
-    }
-    return false;
-}
+// set boundaries for wall
 
-// draw everything to the canvas
 
-function draw(){
-    
-    ctx.drawImage(ground,0,0);
-    
-    for( let i = 0; i < snake.length ; i++){
-        ctx.fillStyle = ( i == 0 )? "green" : "white";
-        ctx.fillRect(snake[i].x,snake[i].y,box,box);
-        
-        ctx.strokeStyle = "red";
-        ctx.strokeRect(snake[i].x,snake[i].y,box,box);
-    }
-    
-    ctx.drawImage(foodImg, food.x, food.y);
-    
-    // old head position
-    let snakeX = snake[0].x;
-    let snakeY = snake[0].y;
-    
-    // which direction
-    if( d == "LEFT") snakeX -= box;
-    if( d == "UP") snakeY -= box;
-    if( d == "RIGHT") snakeX += box;
-    if( d == "DOWN") snakeY += box;
-    
-    // if the snake eats the food
-    if(snakeX == food.x && snakeY == food.y){
-        score++;
-        eat.play();
-        food = {
-            x : Math.floor(Math.random()*17+1) * box,
-            y : Math.floor(Math.random()*15+3) * box
-        }
-        // we don't remove the tail
-    }else{
-        // remove the tail
-        snake.pop();
-    }
-    
-    // add new Head
-    
-    let newHead = {
-        x : snakeX,
-        y : snakeY
-    }
-    
-    // game over
-    
-    if(snakeX < box || snakeX > 17 * box || snakeY < 3*box || snakeY > 17*box || collision(newHead,snake)){
-        clearInterval(game);
-        dead.play();
-    }
-    
-    snake.unshift(newHead);
-    
-    ctx.fillStyle = "white";
-    ctx.font = "45px Changa one";
-    ctx.fillText(score,2*box,1.6*box);
-}
-
-// call draw function every 100 ms
-
-let game = setInterval(draw,100);
+})
